f31f54bb14e9b614be164008589344b3e37679a3
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-22 18:50:56 +0530

    fixes in the type conversion in volatility.sol and the liquiditydistributor.sol

--- logic/src/pool/LiquidityDistributor.sol
+++ logic/src/pool/LiquidityDistributor.sol
@@ -3,17 +3,16 @@ pragma solidity ^0.8.30;
 
 import {DistributionMath} from "../libraries/DistributionMath.sol";
 
-
 contract LiquidityDistributor {
     uint256 public totalLiquidity;
     uint256 public currentTick;
     uint256 public volatilityIndex;
-    uint256 private currentLowerTick;
-    uint256 public constant TICKSPACING = 60 ; //ETH-USDC POOL
+    int24 private currentLowerTick;
+    uint256 public constant TICKSPACING = 60; //ETH-USDC POOL
     uint256[] public weight;
     uint256[] public liquidityDistribution;
 
-    struct SlotPlan{
+    struct SlotPlan {
         int24 lowerTick;
         int24 upperTick;
         uint256 liquidityAmount;
@@ -21,7 +20,11 @@ contract LiquidityDistributor {
 
     SlotPlan[] public slotPlan;
 
-    constructor(uint256 _totalLiquidity, uint256 _currentTick, uint256 _volatilityIndex) {
+    constructor(
+        uint256 _totalLiquidity,
+        uint256 _currentTick,
+        uint256 _volatilityIndex
+    ) {
         totalLiquidity = _totalLiquidity;
         currentTick = _currentTick;
         volatilityIndex = _volatilityIndex;
@@ -29,25 +32,40 @@ contract LiquidityDistributor {
 
     /**
      * @dev Computes the distribution of liquidity across tick ranges based on the current tick and volatility index.
-     * @dev calculates the number of slots to distribute liquidity into based on the volatility index, distributes the total liquidity according to predefined weights, 
+     * @dev calculates the number of slots to distribute liquidity into based on the volatility index, distributes the total liquidity according to predefined weights,
      *       and computes the lower and upper ticks for each slot. The resulting slot plan includes the tick ranges and corresponding liquidity amounts for each slot.
      */
-    function computeDistribution () public returns (SlotPlan[] memory) {
+    function computeDistribution() public returns (SlotPlan[] memory) {
         uint256 numberOfSlots = distributeWeights(volatilityIndex);
 
         liquidityDistribution = new uint256[](numberOfSlots);
-        liquidityDistribution = DistributionMath.Distribute(totalLiquidity, weight);
+        liquidityDistribution = DistributionMath.Distribute(
+            totalLiquidity,
+            weight
+        );
 
         currentLowerTick = computeCurrentLowerTick(currentTick);
 
-        slotPlan = new SlotPlan[](numberOfSlots);
-        for ( uint256 i=0 ; i < numberOfSlots ; i++){
-            slotPlan[i].lowerTick = int24(currentLowerTick + (i - (numberOfSlots -1)/2)*TICKSPACING);
-            slotPlan[i].upperTick = int24(currentLowerTick + (i - (numberOfSlots -1)/2 + 1)*TICKSPACING);
-            slotPlan[i].liquidityAmount = liquidityDistribution[i];
+        SlotPlan[] memory computedPlans = new SlotPlan[](numberOfSlots);
+        int256 center = int256(currentLowerTick);
+        int256 halfWidth = int256((numberOfSlots - 1) / 2);
+        for (uint256 i = 0; i < numberOfSlots; i++) {
+            int256 offset = int256(i) - halfWidth;
+            computedPlans[i].lowerTick = int24(
+                center + (offset * int256(TICKSPACING))
+            );
+            computedPlans[i].upperTick = int24(
+                center + ((offset + 1) * int256(TICKSPACING))
+            );
+            computedPlans[i].liquidityAmount = liquidityDistribution[i];
         }
 
-        return slotPlan;
+        delete slotPlan;
+        for (uint256 i = 0; i < numberOfSlots; i++) {
+            slotPlan.push(computedPlans[i]);
+        }
+
+        return computedPlans;
     }
 
     /**
@@ -55,8 +73,17 @@ contract LiquidityDistributor {
      * @param _volatilityIndex The current volatility index
      * @return n The number of slots to distribute liquidity into
      */
-    function distributeWeights(uint256 _volatilityIndex) internal returns (uint256) {
+    function distributeWeights(
+        uint256 _volatilityIndex
+    ) internal returns (uint256) {
         uint256 n = 3 + (_volatilityIndex - 1) * 2;
         weight = new uint256[](n);
         return n;
+    }
+
+    function computeCurrentLowerTick(
+        uint256 _currentTick
+    ) internal pure returns (int24) {
+        return int24(int256((_currentTick / TICKSPACING) * TICKSPACING));
+    }
 }
--- logic/src/volatility_calc/volatility.sol
+++ logic/src/volatility_calc/volatility.sol
@@ -24,37 +24,27 @@ contract Volatility {
     /* -------------------------------------------------------------------------- */
     /*                                  constants                                 */
     /* -------------------------------------------------------------------------- */
-    uint128 ewmaVariance = 0;
+    uint256 ewmaVariance = 0;
     int24 lastTick;
     uint32 lastObservationTimestamp;
     uint256 lastObservationBlock;
 
     /* ------------------------------- constructur ------------------------------ */
     constructor() {
-        (
-            uint160 sqrtPriceX96,
-            int24 tick,
-            uint24 protocolFee,
-            uint24 lpFee
-        ) = StateLibrary.getSlot0(Config.poolManager, Config.poolId());
+        (, int24 tick,,) = StateLibrary.getSlot0(Config.poolManager, Config.poolId());
 
         lastTick = tick;
-        lastObservationTimestamp = block.timestamp;
+        lastObservationTimestamp = uint32(block.timestamp);
         lastObservationBlock = block.number;
     }
 
     /* -------------------------------------------------------------------------- */
     /*                               public funcions                              */
     /* -------------------------------------------------------------------------- */
-    function calculateEWMA() public view returns (uint256) {
-        (
-            uint160 sqrtPriceX96,
-            int24 m_currenttick,
-            uint24 protocolFee,
-            uint24 lpFee
-        ) = StateLibrary.getSlot0(Config.poolManager, Config.poolId());
+    function calculateEWMA() public returns (uint256) {
+        (, int24 m_currentTick,,) = StateLibrary.getSlot0(Config.poolManager, Config.poolId());
 
-        uint32 m_currentTimestamp = block.timestamp;
+        uint32 m_currentTimestamp = uint32(block.timestamp);
         uint256 m_currentBlock = block.number;
 
         int24 delta = m_currentTick - lastTick;
@@ -72,6 +62,12 @@ contract Volatility {
         }
 
         _calculateEwmaVariance(delta);
+
+        lastTick = m_currentTick;
+        lastObservationTimestamp = m_currentTimestamp;
+        lastObservationBlock = m_currentBlock;
+
+        return ewmaVariance;
     }
 
     /* -------------------------------------------------------------------------- */
@@ -81,10 +77,10 @@ contract Volatility {
         int24 delta,
         uint32 time,
         uint256 bnum
-    ) internal returns (uint8 errorCode) {
+    ) internal view returns (uint8 errorCode) {
         if (bnum <= lastObservationBlock) {
             return 10;
-        } else if (time <= lastObservationTimestamp + Config.Tmin) {
+        } else if (time <= lastObservationTimestamp + Config.TMIN) {
             return 11;
         } else if (delta > 300 || delta < -300) {
             return 1;
@@ -93,8 +89,9 @@ contract Volatility {
         }
     }
 
-    function _calculateEwmaVariance(int24 delta) {
-        uint256 returnSquareScaled = delta * delta * Config.LNSQ_1E18;
+    function _calculateEwmaVariance(int24 delta) internal {
+        int256 delta256 = int256(delta);
+        uint256 returnSquareScaled = uint256(delta256 * delta256) * Config.LNSQ_1E18;
         ewmaVariance =
             Config.LAMBDA *
             ewmaVariance +
