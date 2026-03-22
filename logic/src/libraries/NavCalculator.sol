bfb5d3c30fe838880c75505889b857d6d1a8aa4e
TathagatGupta98 <janaktathagat@gmail.com>
2026-03-22 21:17:27 +0530

    NavCalculatorr: calculating fees in a tick slot.

--- logic/src/libraries/NavCalculator.sol
+++ logic/src/libraries/NavCalculator.sol
@@ -27,6 +27,22 @@ library NavCalculator {
 
         (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(sqrtPriceX96, sqrtPriceLower, sqrtPriceUpper, liquidity)
     }
+    
+    function getPositionUncollectedFees(
+        uint256 tokenId, 
+        int24 tickLower, 
+        int24 tickUpper, 
+        uint128 liquidity
+    ) returns (uint256 feeAmount0, uint256 feeAmount1){
 
+        (uint256 feeGrowthGlobal0X128, uint256 feeGrowthGlobal1X128) = StateLibrary.getPoolFeeGrowthGlobal();
+        (uint256 feeGrowthOutside0X128, uint256 feeGrowthOutside1X128) = StateLibrary.getTickFeeGrowthOutside();
+        (uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128) = StateLibrary.getPositionInfo(); 
+
+        feeGrowthInside0 = feeGrowthGlobal0 - feeGrowthBelowLower0 - feeGrowthAboveUpper0;
+
+        uint256 feeAmount0 = liquidity * (feeGrowthInside0 - feeGrowthInside0Last) / 2**128;
+        uint256 feeAmount1 = liquidity * (feeGrowthInside1 - feeGrowthInside1Last) / 2**128;
+    }
     
 }
