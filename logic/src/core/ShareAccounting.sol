d22bd68157c7629f4cf4363969c53a61ebb84a74
TathagatGupta98 <janaktathagat@gmail.com>
2026-03-22 20:17:46 +0530

    ShareAccounting: Changed variable types and functions call to match types and added validate deposit

--- logic/src/core/ShareAccounting.sol
+++ logic/src/core/ShareAccounting.sol
@@ -26,13 +26,16 @@ pragma solidity ^0.8.30;
 /* --------------------------------- imports -------------------------------- */
 import {VaultStorage} from "./VaultStorage.sol";
 import {Config} from "../helpers/config.sol";
-
+import {PoolId} from "v4-core/src/types/PoolId.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {FullMath} from "v4-core/src/libraries/FullMath.sol";
 
 /* -------------------------------------------------------------------------- */
 /*                                  contract                                  */
 /* -------------------------------------------------------------------------- */
 contract ShareAccounting is VaultStorage{
 
+    using StateLibrary for IPoolManager;
 
 /* ------------------------------- constructor ------------------------------ */
     constructor() VaultStorage(
@@ -47,32 +50,47 @@ contract ShareAccounting is VaultStorage{
 
 
     function getEthUsdcPrice() returns (uint256 p_actual) {
-        uint265 sqrtPriceX96 = poolManager.getSlot0(poolId);
-        uint265 p_raw = (sqrtPriceX96 * sqrtPriceX96) / 2**96;
-        p_actual = p_raw * (10**12);
+        (uint160 sqrtPriceX96, , , ) = poolManager.getSlot0(Config.poolId());
+
+        uint265 p_raw = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) / 2**96;
+        p_actual = p_raw * 1e12;
     }
 
-    function computeDepositValueUsdc(uint256 ethAmount, uint256 usdcAmount) returns(uint256 totalValueUsdc){
+    function computeDepositValueUsdc(uint256 ethAmount, uint256 usdcAmount) internal view returns(uint256 totalValueUsdc){
         uint256 ethPriceUsdc = getEthUsdcPrice();
-        uint256 ethAmountInUsdc = (ethAmount * getEthUsdcPrice)/ETH_DECIMALS;
+        uint256 ethAmountInUsdc = (ethAmount * getEthUsdcPrice) / ETH_DECIMALS;
         totalValueUsdc = usdcAmount + ethAmountInUsdc;
     }
 
-    function computeSharesForDeposit(uint256 depositValueUsd, uint256 currentNavUsdc) returns (uint256 sharesToMint){
+    function computeSharesForDeposit(uint256 depositValueUsd, uint256 currentNavUsdc) internal view returns (uint256 sharesToMint){
         if(initialized){
             sharesToMint = (depositValueUsd * totalShares) / currentNavUsdc;
         }
         else{
+            initialized = true;
             sharesToMint = (depositValueUsd * WAD)/USDC_DECIMALS;    
         } 
     }
+
     function computeTokensForShares(
-        uint256 shareToBurn, 
+        uint256 sharesToBurn, 
         uint256 currentTotalEth, 
         uint256 currentTotalUsdc
-    ) returns(uint256 ethToReturn, uint256 usdcToReturn){
+    ) internal view returns(uint256 ethToReturn, uint256 usdcToReturn){
+
         ethToReturn  = (sharesToBurn * currentTotalEth)  / totalShares;
         usdcToReturn = (sharesToBurn * currentTotalUsdc) / totalShares;
+    }
+
+    function validateDeposit(
+        uint256 ethAmount,
+        uint256 usdcAmount,
+        uint256 depositValueUsdc
+    ) internal view {
+        if (ethAmount == 0 && usdcAmount == 0) revert ZeroDeposit();
+        if (depositValueUsdc < MIN_DEPOSIT_USDC)
+            revert BelowMinimumDeposit(depositValueUsdc, MIN_DEPOSIT_USDC);
+        if (paused) revert VaultPaused();
     }
 
 }
