40d84ece62b258873c5dcfecc25aa1223dce127d
Uk2476 <kuttkarsh028@gmail.com>
2026-03-22 18:41:07 +0530

    LiquidityDistributors.sol : function get currentlowerslot tick value , added few conditions in constructor , and also added custon errors

--- logic/src/pool/LiquidityDistributor.sol
+++ logic/src/pool/LiquidityDistributor.sol
@@ -5,13 +5,18 @@ import {DistributionMath} from "../libraries/DistributionMath.sol";
 
 
 contract LiquidityDistributor {
+    error LiquidityMustBeGreaterThanZero();
+    error VolatilityIndexMustBeGreaterThanZero();
+    error CurrentTickOutOfBounds();
+
     uint256 public totalLiquidity;
     uint256 public currentTick;
     uint256 public volatilityIndex;
     uint256 private currentLowerTick;
     uint256 public constant TICKSPACING = 60 ; //ETH-USDC POOL
+    uint256 public constant MAX_TICK = 887272;
+    uint256 public constant MIN_TICK = -887272; 
     uint256[] public weight;
-    uint256[] public liquidityDistribution;
 
     struct SlotPlan{
         int24 lowerTick;
@@ -22,6 +27,16 @@ contract LiquidityDistributor {
     SlotPlan[] public slotPlan;
 
     constructor(uint256 _totalLiquidity, uint256 _currentTick, uint256 _volatilityIndex) {
+        if(_totalLiquidity == 0){
+            revert LiquidityMustBeGreaterThanZero();
+        }
+        if(_volatilityIndex == 0){
+            revert VolatilityIndexMustBeGreaterThanZero();
+        }
+        if (_currentTick < MIN_TICK || _currentTick > MAX_TICK){
+            revert CurrentTickOutOfBounds();
+        }
+
         totalLiquidity = _totalLiquidity;
         currentTick = _currentTick;
         volatilityIndex = _volatilityIndex;
@@ -34,11 +49,12 @@ contract LiquidityDistributor {
      */
     function computeDistribution () public returns (SlotPlan[] memory) {
         uint256 numberOfSlots = distributeWeights(volatilityIndex);
+        uint256[] memory liquidityDistribution;
 
         liquidityDistribution = new uint256[](numberOfSlots);
         liquidityDistribution = DistributionMath.Distribute(totalLiquidity, weight);
 
-        currentLowerTick = computeCurrentLowerTick(currentTick);
+        currentLowerTick = getCurrentLowerTick(currentTick);
 
         slotPlan = new SlotPlan[](numberOfSlots);
         for ( uint256 i=0 ; i < numberOfSlots ; i++){
@@ -49,6 +65,16 @@ contract LiquidityDistributor {
 
         return slotPlan;
     }
+
+
+    /**
+     * @dev Calculates the current lower tick based on the current tick and tick spacing.
+     * @return The current lower tick
+     */
+    function getCurrentLowerTick(uint256 _currentTick) internal pure returns (uint256) {
+        return (_currentTick - (_currentTick % TICKSPACING));
+    }
+
 
     /**
      * @dev calculate the number of slots to distribute liquidity into based on the volatility index. and initializes the weight array accordingly.
