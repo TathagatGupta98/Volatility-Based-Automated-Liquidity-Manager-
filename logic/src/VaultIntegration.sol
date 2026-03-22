e09d70a4af08b9ace9a28ab53a8a9a8fec2748c8
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-23 03:48:32 +0530

    done with writing the checkupkeep and the performupkeep functions

--- logic/src/VaultIntegration.sol
+++ logic/src/VaultIntegration.sol
@@ -12,35 +12,98 @@ import {Volatility} from "./volatility_calc/volatility.sol";
 
 interface IPoolInteractorVolatility {
     function setVolatilityIndex(uint8 newVolatilityIndex) external;
+    function rebalance(uint256 totalLiquidityAvailable, uint256 _volatilityIndex) external;
+    function needsTickDriftRebalance() external view returns (bool);
 }
 
 contract VaultIntegration is AutomationCompatibleInterface, Volatility {
 /* --------------------------------- errors --------------------------------- */
     error VeryFrequentUpkeep();
-    error NoChangeInVolatilityIndex();
     error NotAuthorized();
+    error PoolInteractorNotSet();
 /* -------------------------------------------------------------------------- */
 /*                               state variables                              */
 /* -------------------------------------------------------------------------- */
     uint public lastTimestamp;
-    uint public interval = 300; 
-    uint8 lastVolatilityIndex;
+    uint public interval = 300;
+    uint8 public lastVolatilityIndex;
     address public owner;
     IPoolInteractorVolatility public poolInteractor;
+    uint256 public rebalanceLiquidity;
+    bool public autoRebalanceEnabled = true;
+    bool public driftRebalanceEnabled = true;
 
     modifier onlyOwner() {
         if (msg.sender != owner) revert NotAuthorized();
         _;
     }
 /* ------------------------------- constructor ------------------------------ */
-    constructor() Volatility(){
+    constructor() Volatility() {
         lastTimestamp = block.timestamp;
         owner = msg.sender;
+        lastVolatilityIndex = getVolatilityIndex();
     }
 
     function setPoolInteractor(address poolInteractorAddress) external onlyOwner {
         poolInteractor = IPoolInteractorVolatility(poolInteractorAddress);
     }
+
+    function setInterval(uint256 newInterval) external onlyOwner {
+        interval = newInterval;
+    }
+
+    function setRebalanceLiquidity(uint256 newRebalanceLiquidity) external onlyOwner {
+        rebalanceLiquidity = newRebalanceLiquidity;
+    }
+
+    function setAutoRebalanceEnabled(bool enabled) external onlyOwner {
+        autoRebalanceEnabled = enabled;
+    }
+
+    function setDriftRebalanceEnabled(bool enabled) external onlyOwner {
+        driftRebalanceEnabled = enabled;
+    }
+
+    function syncVolatilityIndex() public returns (uint8 currentVolatilityIndex, uint256 currentVolatilityValue) {
+        if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
+
+        calculateVolatility();
+        currentVolatilityIndex = getVolatilityIndex();
+        currentVolatilityValue = getVolatilityValue();
+
+        if (currentVolatilityIndex != lastVolatilityIndex) {
+            lastVolatilityIndex = currentVolatilityIndex;
+        }
+
+        poolInteractor.setVolatilityIndex(currentVolatilityIndex);
+    }
+
+    function rebalanceNow(uint256 totalLiquidityAvailable) external onlyOwner {
+        if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
+        (uint8 currentVolatilityIndex,) = syncVolatilityIndex();
+        poolInteractor.rebalance(totalLiquidityAvailable, currentVolatilityIndex);
+    }
+
+    function executeEngineCycle() external {
+        if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
+
+        uint8 previousVolatilityIndex = lastVolatilityIndex;
+        (uint8 currentVolatilityIndex,) = syncVolatilityIndex();
+
+        bool shouldRebalance = false;
+        if (autoRebalanceEnabled) {
+            shouldRebalance = currentVolatilityIndex != previousVolatilityIndex;
+        }
+
+        if (driftRebalanceEnabled) {
+            shouldRebalance = shouldRebalance || poolInteractor.needsTickDriftRebalance();
+        }
+
+        if (shouldRebalance && rebalanceLiquidity > 0) {
+            poolInteractor.rebalance(rebalanceLiquidity, currentVolatilityIndex);
+        }
+    }
+
 /* ------------------------------- checkUpKeep ------------------------------ */
     function checkUpkeep(bytes calldata checkData)
         external
@@ -48,27 +111,48 @@ contract VaultIntegration is AutomationCompatibleInterface, Volatility {
         override
         returns (bool upkeepNeeded, bytes memory performData)
     {
-        upkeepNeeded = (block.timestamp - lastTimestamp) >= interval;
+        bool timeElapsed = (block.timestamp - lastTimestamp) >= interval;
+        bool driftNeedsRebalance = false;
+
+        if (driftRebalanceEnabled && address(poolInteractor) != address(0)) {
+            try poolInteractor.needsTickDriftRebalance() returns (bool needed) {
+                driftNeedsRebalance = needed;
+            } catch {
+                driftNeedsRebalance = false;
+            }
+        }
+
+        upkeepNeeded = timeElapsed || driftNeedsRebalance;
         performData = checkData;
     }
 /* ------------------------------ performUpKeep ----------------------------- */
-    function performUpkeep(bytes calldata) external override {
-        if ((block.timestamp - lastTimestamp) >= interval) {
-            lastTimestamp = block.timestamp;
-            calculateVolatility();
-            uint8 m_currentVolatility = getVolatilityIndex();
-            if (m_currentVolatility != lastVolatilityIndex) {
-                lastVolatilityIndex = m_currentVolatility;
-                if (address(poolInteractor) != address(0)) {
-                    poolInteractor.setVolatilityIndex(m_currentVolatility);
-                }
-            }
-            else {
-                revert NoChangeInVolatilityIndex();
-            }
-        }
-        else {
+    function performUpkeep(bytes calldata performData) external override {
+        if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
+
+        bool timeElapsed = (block.timestamp - lastTimestamp) >= interval;
+        bool driftNeedsRebalance = driftRebalanceEnabled && poolInteractor.needsTickDriftRebalance();
+
+        if (!timeElapsed && !driftNeedsRebalance) {
             revert VeryFrequentUpkeep();
+        }
+
+        if (timeElapsed) {
+            lastTimestamp = block.timestamp;
+        }
+
+        uint256 liquidityToUse = rebalanceLiquidity;
+        if (performData.length == 32) {
+            liquidityToUse = abi.decode(performData, (uint256));
+        }
+
+        uint8 previousVolatilityIndex = lastVolatilityIndex;
+        (uint8 currentVolatilityIndex,) = syncVolatilityIndex();
+
+        bool indexChanged = currentVolatilityIndex != previousVolatilityIndex;
+        bool shouldRebalance = (autoRebalanceEnabled && indexChanged) || driftNeedsRebalance;
+
+        if (shouldRebalance && liquidityToUse > 0) {
+            poolInteractor.rebalance(liquidityToUse, currentVolatilityIndex);
         }
     }
 }
