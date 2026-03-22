5e89f5a6fe8ee18093cffa528d39b8b2357e76cd
Uk2476 <kuttkarsh028@gmail.com>
2026-03-22 21:07:01 +0530

    positionTracker.sol : defined all the function in this positionTraker

--- logic/src/pool/PositionTracker.sol
+++ logic/src/pool/PositionTracker.sol
@@ -10,7 +10,6 @@ contract PoolInteractor {
         uint256 tokenId;
         int256 lowerTick;
         int256 upperTick;
-        uint256 liquidityAmount;
         bool isActive;
     }
 
@@ -23,22 +22,34 @@ contract PoolInteractor {
         }
         _;
     }
-
-    function setSlotState(uint slotIndex, SlotState memory state) public onlyPoolInteractor {
+    // @dev define the slot state
+    function setSlotState(uint256 slotIndex,uint256 _tokenId , int256 _lowerTick, int256 _upperTick, uint256 _liquidityAmount, bool _isActive) public onlyPoolInteractor {
+        slots[slotIndex] = SlotState({
+            tokenId: _tokenId,
+            lowerTick: _lowerTick,
+            upperTick: _upperTick,
+            isActive: _isActive
+        });
+    }
+    // @dev called when we make liquidity holding of a position inactive or active
+    function setHasLiquidity(uint256 slotIndex, bool hasLiquidity) public onlyPoolInteractor {
+        slots[slotIndex].isActive = hasLiquidity;
     }
 
-    function setHasLiquidity(uint slotIndex, bool hasLiquidity) public onlyPoolInteractor {
-    }
-
+    // @dev volatility index changes and we need to change the slot count accordingly
     function setSlotCount(uint256 newSlotCount) public onlyPoolInteractor {
+        slotCount = newSlotCount;
     }
 
     function getSlotState(uint slotIndex) public view returns (SlotState memory) {
+        return slots[slotIndex];
     }
 
     function hasLiquidity(uint slotIndex) public view returns (bool) {
+        return slots[slotIndex].isActive;
     }
 
     function getTokenId(uint slotIndex) public view returns (uint256) {
+        return slots[slotIndex].tokenId;
     }
 }
