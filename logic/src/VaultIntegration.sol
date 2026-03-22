3ee421b1ae90cd04f3b530fb9c16fb2adf1658fa
Aradhya Agrawal <aradhya.gzb08@gmail.com>
2026-03-23 03:57:04 +0530

    finalizing the vault integration file

--- logic/src/VaultIntegration.sol
+++ logic/src/VaultIntegration.sol
@@ -7,13 +7,23 @@ pragma solidity ^0.8.30;
  * @notice This contract is designed to integrate with Chainlink Automation to trigger the volatility calculation and liquidity distribution processes in the vault system at regular intervals. It implements the AutomationCompatibleInterface, allowing it to be registered as an upkeep contract with Chainlink Automation.
  */
 
-import "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
 import {Volatility} from "./volatility_calc/volatility.sol";
+
+interface AutomationCompatibleInterface {
+    function checkUpkeep(bytes calldata checkData)
+        external
+        returns (bool upkeepNeeded, bytes memory performData);
+
+    function performUpkeep(bytes calldata performData) external;
+}
 
 interface IPoolInteractorVolatility {
     function setVolatilityIndex(uint8 newVolatilityIndex) external;
     function rebalance(uint256 totalLiquidityAvailable, uint256 _volatilityIndex) external;
     function needsTickDriftRebalance() external view returns (bool);
+    function owner() external view returns (address);
+    function volatilityUpdater() external view returns (address);
+    function volatilityIndex() external view returns (uint8);
 }
 
 contract VaultIntegration is AutomationCompatibleInterface, Volatility {
@@ -21,6 +31,7 @@ contract VaultIntegration is AutomationCompatibleInterface, Volatility {
     error VeryFrequentUpkeep();
     error NotAuthorized();
     error PoolInteractorNotSet();
+    error InvalidOwnerAddress();
 /* -------------------------------------------------------------------------- */
 /*                               state variables                              */
 /* -------------------------------------------------------------------------- */
@@ -46,6 +57,11 @@ contract VaultIntegration is AutomationCompatibleInterface, Volatility {
 
     function setPoolInteractor(address poolInteractorAddress) external onlyOwner {
         poolInteractor = IPoolInteractorVolatility(poolInteractorAddress);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        if (newOwner == address(0)) revert InvalidOwnerAddress();
+        owner = newOwner;
     }
 
     function setInterval(uint256 newInterval) external onlyOwner {
@@ -78,10 +94,54 @@ contract VaultIntegration is AutomationCompatibleInterface, Volatility {
         poolInteractor.setVolatilityIndex(currentVolatilityIndex);
     }
 
+    function previewSyncVolatilityIndex() external view returns (uint8 currentVolatilityIndex, uint256 currentVolatilityValue) {
+        currentVolatilityIndex = getVolatilityIndex();
+        currentVolatilityValue = getVolatilityValue();
+    }
+
+    function getIntegrationStatus()
+        external
+        view
+        returns (
+            bool poolInteractorConfigured,
+            bool canVaultUpdateVolatilityIndex,
+            bool canVaultCallRebalance,
+            uint8 configuredPoolVolatilityIndex,
+            bool driftNeedsRebalance,
+            uint256 configuredRebalanceLiquidity,
+            uint256 configuredInterval
+        )
+    {
+        poolInteractorConfigured = address(poolInteractor) != address(0);
+        configuredRebalanceLiquidity = rebalanceLiquidity;
+        configuredInterval = interval;
+
+        if (!poolInteractorConfigured) {
+            return (false, false, false, 0, false, configuredRebalanceLiquidity, configuredInterval);
+        }
+
+        address poolOwner = poolInteractor.owner();
+        address updater = poolInteractor.volatilityUpdater();
+        configuredPoolVolatilityIndex = poolInteractor.volatilityIndex();
+
+        canVaultUpdateVolatilityIndex = updater == address(this) || poolOwner == address(this);
+        canVaultCallRebalance = poolOwner == address(this);
+
+        try poolInteractor.needsTickDriftRebalance() returns (bool needed) {
+            driftNeedsRebalance = needed;
+        } catch {
+            driftNeedsRebalance = false;
+        }
+    }
+
     function rebalanceNow(uint256 totalLiquidityAvailable) external onlyOwner {
         if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
         (uint8 currentVolatilityIndex,) = syncVolatilityIndex();
         poolInteractor.rebalance(totalLiquidityAvailable, currentVolatilityIndex);
+    }
+
+    function pushVolatilityIndexOnly() external onlyOwner returns (uint8 currentVolatilityIndex, uint256 currentVolatilityValue) {
+        return syncVolatilityIndex();
     }
 
     function executeEngineCycle() external {
@@ -101,6 +161,26 @@ contract VaultIntegration is AutomationCompatibleInterface, Volatility {
 
         if (shouldRebalance && rebalanceLiquidity > 0) {
             poolInteractor.rebalance(rebalanceLiquidity, currentVolatilityIndex);
+        }
+    }
+
+    function executeEngineCycleWithLiquidity(uint256 liquidityToUse) external {
+        if (address(poolInteractor) == address(0)) revert PoolInteractorNotSet();
+
+        uint8 previousVolatilityIndex = lastVolatilityIndex;
+        (uint8 currentVolatilityIndex,) = syncVolatilityIndex();
+
+        bool shouldRebalance = false;
+        if (autoRebalanceEnabled) {
+            shouldRebalance = currentVolatilityIndex != previousVolatilityIndex;
+        }
+
+        if (driftRebalanceEnabled) {
+            shouldRebalance = shouldRebalance || poolInteractor.needsTickDriftRebalance();
+        }
+
+        if (shouldRebalance && liquidityToUse > 0) {
+            poolInteractor.rebalance(liquidityToUse, currentVolatilityIndex);
         }
     }
 
